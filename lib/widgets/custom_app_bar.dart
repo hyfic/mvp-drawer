9d20dee614db7c31c642cd3fe938ceac87b8e37a
fadhilsaheer <fadhilsaheer@gmail.com>
2021-09-24 20:34:38 +0530

    app bar

---
+++ lib/widgets/custom_app_bar.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:hyfic_mvp_drawer/utils/colors.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: appSecondaryColor,
+      padding: EdgeInsets.all(20.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          InkWell(
+            onTap: () {},
+            child: Image.asset('images/logo.png', width: 30.0),
+          ),
+          const CircleAvatar(
+            backgroundImage: NetworkImage(
+              'https://pbs.twimg.com/profile_images/1374233832057053190/xftrnScS.jpg',
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
