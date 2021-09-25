aa2706b118e6f4a5fd730ca20c29c2821ee10f81
fadhilsaheer <fadhilsaheer@gmail.com>
2021-09-26 04:29:13 +0530

    added profile

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,28 +10,61 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: appPrimaryColor,
       drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: [
-            IconButton(
-              onPressed: () {},
-              icon: Icon(Icons.home_outlined),
-            ),
-            IconButton(
-              onPressed: () {},
-              icon: Icon(Icons.home_outlined),
-            ),
-            IconButton(
-              onPressed: () {},
-              icon: Icon(Icons.home_outlined),
-            ),
-          ],
+        child: Container(
+          color: appSecondaryColor,
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: [
+              DrawerHeader(
+                decoration: const BoxDecoration(
+                  color: appPrimaryColor,
+                  image: DecorationImage(
+                    image: NetworkImage(
+                      'https://i.insider.com/602ee9e3d3ad27001837f2af?width=1200&format=jpeg',
+                    ),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                padding: EdgeInsets.zero,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Container(
+                      width: double.infinity,
+                      padding: EdgeInsets.all(10.0),
+                      color: Colors.black.withOpacity(0.5),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Rick Astley',
+                            style: TextStyle(
+                              fontSize: 25.0,
+                              color: appTextColor,
+                            ),
+                          ),
+                          Text(
+                            '@rick',
+                            style: TextStyle(
+                              color: appTextOpacityColor,
+                              fontSize: 18.0,
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
       body: SafeArea(
         child: SingleChildScrollView(
           child: Column(
-            children: [
+            children: const [
               CustomAppBar(),
             ],
           ),
